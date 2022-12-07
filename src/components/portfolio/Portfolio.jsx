fc2ddecd15f32f8c97fbbdfb2fe514e2544790fd
Marwan Summakieh <42711091+MarwanSummakieh@users.noreply.github.com>
2022-12-07 17:59:28 +0100

    getting the porjects directly from github

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,63 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./portfolio.css";
-import portfolio from "../../assets/portfolio.svg";
-import emergencyButton from "../../assets/EmergencyButton.svg";
-import artillery from "../../assets/artillery.svg";
-import mail from "../../assets/mail.svg";
 function Portfolio() {
-  const portfolioData = [
-    {
-      id: 1,
-      title: "Portfolio Website",
-      about: "This is my portfolio website",
-      githubLink: "https://github.com/MarwanSummakieh/portfolioreact",
-      image: portfolio,
-    },
-    {
-      id: 2,
-      title: "Emergency App",
-      about: "My Bacherlor's Degree Project",
-      githubLink: "https://github.com/MarwanSummakieh/bpr-app",
-      image: emergencyButton,
-    },
-    {
-      id: 3,
-      title: "Strategy Game with Unity",
-      about: "An attempt to recreate the game Generals",
-      githubLink: "https://github.com/MarwanSummakieh/real-time-strategie",
-      image: artillery,
-    },
-    {
-      id: 4,
-      title: "Mail App",
-      about: "Mail app that uses 3-tier architecture",
-      githubLink: "https://github.com/MarwanSummakieh/Small-Messaging-App",
-      image: mail,
-    },
-  ];
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    const reposURl = `https://api.github.com/users/MarwanSummakieh/repos`;
+    const getRepos = async () => {
+      const response = await fetch(reposURl);
+      const repos = await response.json();
+      setRepos(repos);
+    };
+    getRepos();
+  }, []);
+
   return (
     <section id="portfolio">
       <h5>Some of my work</h5>
       <h2>Portfolio</h2>
       <div className="portfolio_container">
-      {portfolioData.map(({ id, image, title, about, githubLink }) => {
-        return (
-          <article key={id} className="portfolio_item">
-            <div className="portfolio_item_image">
-              <img src={image} alt="img" />
-            </div>
-            <h3>{title}</h3>
-            <p>{about}</p>
-            <a
-              href={githubLink}
-              className="btn portfolio_item_cta"
-              target={"noreferrer"}
-            >
-              Github Link
-            </a>
-          </article>
-        );
-      })}
+        {repos.map((repo) => {
+          return (
+            <section id={repo.id}>
+              <div className="portfolio_item">
+                <h5 className="name">{repo.name}</h5>
+                <a href={repo.html_url} target="_blank" rel="noreferrer">
+                  <button className="btn btn-primary">github link</button>
+                </a>
+              </div>
+            </section>
+          );
+        })}
       </div>
     </section>
   );
